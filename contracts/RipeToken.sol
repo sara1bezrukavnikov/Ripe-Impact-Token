67628bb796c2efb8dcc687f3c9b4996dace03161
Sara Bezrukavnikov <sara.bezrukavnikov@gmail.com>
2018-02-17 22:26:47 -0700

    Update the RipeToken file

--- contracts/RipeToken.sol
+++ contracts/RipeToken.sol
@@ -1,80 +1,51 @@
 pragma solidity ^0.4.20;
 
 contract RipeToken {
+    address public donor;
+    address public resquer;
+    uint public weight;
     
-    struct Donation {
-        address donatorId;
-        address volunteerOrgId;
-        uint lbsDonated;
-        bool verified;
+    enum State { InTransit, Received, Aborted }
+    State public state;
+    
+    event Aborted();
+    event FoodOnItsWay();
+    event ItemReceived();
+    
+    modifier onlyResquer() {
+        require(msg.sender == resquer);
+        _;
     }
-    
-    struct Donator {
-        address id;
-        string name;
-    }
-    
-    struct VolunteerOrg {
-        address id;
-        string name;
-    }
-    
-    Donator[] public Donators;
-    mapping(address => uint) DonatorToIndex;
-    
-    VolunteerOrg[] public VolunteerOrgs;
-    mapping(address => uint) VolunteerOrgToIndex;
-    
-    Donation[] public UnverifiedDonations;
-    
-    function newDonator(string _name, address _donatorAddress) public {
-        uint donatorIndex = Donators.push(Donator({
-            id: msg.sender,
-            name: _name
-        }));
-        DonatorToIndex[_donatorAddress] = donatorIndex;
-    }
-    
-    function getDonators() public view returns(Donator[]) {
-        return Donators;
-    }
-    
-    function newVolunteerOrg(string _name, address _volunteerOrgAddress) public {
-        uint volunteerOrgIndex = VolunteerOrgs.push(VolunteerOrg({
-            id: msg.sender,
-            name: _name
-        }));
-        VolunteerOrgToIndex[_volunteerOrgAddress] = volunteerOrgIndex;
-    }
-    
-    function getVolunteerOrgs() public view returns (VolunteerOrg[]) {
-        return VolunteerOrgs;
-    }
-    
-    function isVolunteerOrg(address _volunteerOrgId) public view returns (bool) {
-        uint index = VolunteerOrgToIndex[_volunteerOrgId];
-        if (VolunteerOrgs[index].id == _volunteerOrgId) {
-            return true;
-        }
-        return false;
-    }
-    
-    function newDonation(address _volunteerOrgId, uint _lbsDonated) public returns (uint) {
-        uint donationId = UnverifiedDonations.push(Donation({
-            donatorId: msg.sender,
-            volunteerOrgId: _volunteerOrgId,
-            lbsDonated: _lbsDonated,
-            verified: false
-        }));
-        return donationId;
-    }
-    
-    function isDonator(address _donatorId) public view returns (bool) {
-        uint index = DonatorToIndex[_donatorId];
-        if (Donators[index].id == _donatorId) {
-            return true;
-        }
-        return false;
-    }
-}
 
+    modifier onlyDonor() {
+        require(msg.sender == donor);
+        _;
+    }
+    
+    //The request to donate the food is created 
+    function Donate() public payable 
+    onlyDonor {
+        weight = msg.value;
+        donor = msg.sender;
+        state = State.InTransit;
+        FoodOnItsWay();
+
+    }
+    
+    //Ordered is cancelled
+    function Abort() onlyDonor 
+        public
+        onlyDonor
+    {
+        state = State.Aborted;
+        Aborted();
+    }
+    //Order delivered to the resquer
+    function orderDelivered() public payable
+    onlyResquer {
+        state = State.Received;
+        resquer.transfer(weight);
+        donor.transfer(weight);
+        ItemReceived();
+    }
+    }
